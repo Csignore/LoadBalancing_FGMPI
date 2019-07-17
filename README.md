f27b453c8b9d0b44e4eaae843716ac3910f6edc2
Csignore <ychenvin@gmail.com>
2019-07-17 10:07:53 -0700

    Beautifize lb.c; update readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Load Balancing project based on FGMPI
 
 ## Introduction
-MPI processors, individually, perform a load of tasks. Generally, the efficiency of the MPI program depends on the slowest processor in the program; however, the task is not equally distributed among the processors,  which means that a fast and reliable load balancing mechanism in MPI can remarkably increase the efficiency.
+MPI processors, individually, perform a load of tasks. Generally, the efficiency of the MPI program depends on the slowest processor in the program; however, the tasks are not equally distributed among the processors,  which means that a fast and reliable load balancing mechanism in MPI can remarkably increase the efficiency.
 
 ### Example:
 Three processors A,B,C
@@ -13,7 +13,7 @@ Nine computational processes/tasks 0 to 8: task 0 requires much more time than t
 ## Mechanism
 The library [FGMPI](https://www.cs.ubc.ca/~humaira/fgmpi.html) allows for interleaved execution of multiple concurrent MPI processes inside an OS-process. In this load balacing project, all  concurrent MPI processes in each OS-process are assigned to a role: a manager process and the rest are workers.
 
-The manager process is in charge in assigning task to every workers that belong to the same OS-process, which allows the workload of each worker processes to evenly distribute. In addition, the communnication between the managers allows the work of each OS-process movable, which likely to minimum the total running time of the program.
+The manager process is in charge of assigning task to every workers that belong to the same OS-process, which allows the workload of each worker processes to evenly distribute. In addition, the communnication between the managers allows the work of each OS-process movable, which likely to minimum the total running time of the program.
 
 
 
--- lb.c
+++ lb.c
@@ -54,40 +54,18 @@ FG_ProcessPtr_t binding_func(int argc, char** argv, int rank)
 }
 
 int FG_Process(int argc, char **argv){
-	int rank, size;
+	int rank, size, osProcNum, osProcSize, startRank, proc_color, mag_color = 0;
 	MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
-
-    int osProcNum, osProcSize, startRank, proc_color, mag_color = 0;
     MPIX_Get_collocated_size(&osProcSize);
     MPIX_Get_n_size(&osProcNum);
     MPIX_Get_collocated_startrank(&startRank);
-    //printf("this %d processes has size %d and has %d procNum\n", rank, osProcSize, osProcNum);
-
-    proc_color = rank / osProcSize; 
-
+    proc_color = rank / osProcSize;
+    mag_color =  (rank == startRank) ? 1 : 0;
     MPI_Comm proc_comm, mag_comm;
-    char name[12], nameout[12];
-    int rlen;
-    snprintf(name, 12, "comm_%d", proc_color);
-    //printf("this %d processes has proc_color %d\n", rank, proc_color);
     MPI_Comm_split(MPI_COMM_WORLD, proc_color, rank, &proc_comm);
-    MPI_Comm_set_name( proc_comm, name );
-
-    
-    char magName[12];
-    if (rank == startRank) {
-    	mag_color = 1;
-    	snprintf(magName, 12, "magComm");
-    } else {snprintf(magName, 12, "workerComm");}
     MPI_Comm_split(MPI_COMM_WORLD, mag_color, rank, &mag_comm);
-    MPI_Comm_set_name( mag_comm, magName );
-    //nameout[0] = 0;
-    //MPI_Comm_get_name( mag_comm, nameout, &rlen );
-    //printf("this %d processes belongs to mag_comm %s\n", rank, nameout);
-
 
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -99,33 +77,26 @@ int FG_Process(int argc, char **argv){
     if (mag_color) {
     	
     	// this is the case for manager processes;
-		int replyreqbuf = 0;
-		int reqbuf = 0;
-	    int notdoneSign = TRUE;
-	    int ownWorkdone = FALSE;
-	    int helpOffering = FALSE;
+		int replyreqbuf = 0, reqbuf = 0;
+	    int notdoneSign = TRUE, ownWorkdone = FALSE, helpOffering = FALSE;
+
 
 	    // for managers communication
 	    MPI_Request helprequest;
-    	int prevmag, nextmag;
+    	int prevmag, nextmag, helpComing = TRUE;
         who_are_my_magNeighb(rank, osProcNum, osProcSize, &prevmag, &nextmag);
-        int helpComing = TRUE;
 	    MPI_Irecv(&helpComing,1,MPI_INT,prevmag,HELP_TAG,mag_comm,&helprequest);
     	
 
     	while (notdoneSign){
 
-
+    		// check if any help is offered
     		MPI_Status helpStatus;
     		int helpReqRes=FALSE;
 			MPI_Test(&helprequest,&helpReqRes,&helpStatus);
-            if ( helpReqRes == TRUE) {
-            	printf("manager_rank %d is requesting work from rank %d\n", prevmag*osProcSize, rank);
-            	if (ownWorkdone == TRUE) {
-            		replyreqbuf = -1;
-            	} else {replyreqbuf++;}
+            if (helpReqRes) {
+            	replyreqbuf = (ownWorkdone) ? -1 : replyreqbuf+1;
             	MPI_Send(&replyreqbuf, 1, MPI_INT, prevmag, HELPREP_TAG, mag_comm);
-            	printf("rank %d is sending work %d back the prevmag\n", rank, replyreqbuf);
             };
 
 
@@ -133,113 +104,69 @@ int FG_Process(int argc, char **argv){
 
 	    	MPI_Status status;
 			MPI_Recv(&reqbuf, 1, MPI_INT, MPI_ANY_SOURCE, WORKREQ_TAG, proc_comm, &status);
-	        //printf("the manager has received request from worker %d, value is %d.\n",status.MPI_SOURCE, reqbuf);
 	        if (reqbuf == -1 || ownWorkdone == TRUE) {
-
-
 	        	// communicate with other managers
-	        	printf("rank %d surely has its work done\n", rank);
+	        	if (ownWorkdone == FALSE) printf("proc_comm with manager %d has finished their own job.\n", rank);
 	        	ownWorkdone = TRUE;
-
-	        	if (helpOffering == FALSE) {
+				if (!helpOffering) {
 	        		helpOffering = TRUE;
 	        		MPI_Send(&helpComing, 1, MPI_INT, nextmag, HELP_TAG, mag_comm);
-	        		printf("requesting the work since rank %d is out\n", rank);
-
-
+	        		
 	        		// Case  when all the processes happend to finish at the same time
-					int temphelpReqRes=FALSE; MPI_Status temphelpStatus;
+					int temphelpReqRes=FALSE; 
+					MPI_Status temphelpStatus;
 		        	MPI_Test(&helprequest,&temphelpReqRes,&temphelpStatus);
-	            	if ( temphelpReqRes == TRUE) {
+	            	if (temphelpReqRes) {
 	            		replyreqbuf = -1;
-		        		printf("this case is when all the processes happend to finish at the same time\n");
 		        		MPI_Send(&replyreqbuf, 1, MPI_INT, prevmag, HELPREP_TAG, mag_comm);
 		        	}
 	
 		        	MPI_Status tempSta;
 		        	MPI_Recv(&replyreqbuf, 1, MPI_INT, nextmag, HELPREP_TAG, mag_comm, &tempSta);
 		        	helpOffering = FALSE;
-		        	printf("rank %d is receiving replyreqbuf %d\n", rank, replyreqbuf);
 		        	if (replyreqbuf > MAXINT || replyreqbuf == -1){
-		       printf("rank %d got replyreqbuf %d and proceed to end\n", rank, replyreqbuf);
-		        		noticeMyPrevMag(prevmag, mag_comm);
-		       printf("rank %d ending process 1\n", rank);
-			        	noticeMyworkers(osProcSize, proc_comm);
-			   printf("rank %d ending process 2\n", rank);
-			        	finishSendfromworkers(osProcSize, proc_comm, status.MPI_SOURCE);
-			   printf("rank %d ending process 3\n", rank);
-			        	notdoneSign = FALSE; replyreqbuf = -1;
+		       			noticeMyPrevMag(prevmag, mag_comm);
+		       			noticeMyworkers(osProcSize, proc_comm);
+						finishSendfromworkers(osProcSize, proc_comm, status.MPI_SOURCE);
+						notdoneSign = FALSE; replyreqbuf = -1;
 			        	MPI_Request tempReq;
-			        	printf("the manager %d still send replyreqbuf %d to rank %d\n", rank, replyreqbuf, status.MPI_SOURCE);
 			        	MPI_Isend(&replyreqbuf, 1, MPI_INT, status.MPI_SOURCE, WORK_TAG, proc_comm, &tempReq);
-			        	printf("the manager %d still send replyreqbuf %d to rank %d\n", rank, replyreqbuf, status.MPI_SOURCE);
-						
 		        	} else{
+		        		printf("The worker %d is helping the other proc_comm doing work No. %d.\n", status.MPI_SOURCE, replyreqbuf);
 		        		MPI_Send(&replyreqbuf, 1, MPI_INT, status.MPI_SOURCE, WORK_TAG, proc_comm);
-						printf("the manager has sent a work from nextmag a replyreqbuf as value %d to worker %d \n", replyreqbuf, status.MPI_SOURCE+startRank);	
 		        	}
-
-
-
-
 	        	} else{
-	        		printf("the manager %d is offering help now, come back later\n", rank);
 	        		MPI_Send(&reqbuf, 1, MPI_INT, status.MPI_SOURCE, WORK_TAG, proc_comm);
 	        	}
-	        	
-
-
-
-
-
-
-
 	        } else{
-	        	// send reply back to sender of the request received above
-				replyreqbuf++;
+	        	replyreqbuf++;
 				MPI_Send(&replyreqbuf, 1, MPI_INT, status.MPI_SOURCE, WORK_TAG, proc_comm);
-				//printf("the manager has sent the replyreqbuf as value %d to worker %d \n", replyreqbuf, status.MPI_SOURCE);	
 	        }
 	    }
-	    //noticeMyworkers(osProcSize, proc_comm);
-	    printf("rank %d ending process 4\n", rank);
     	getNoticefromworkers(osProcSize, proc_comm);
-    	printf("mag %d has reached here===============\n", rank);
     	MPI_Barrier(mag_comm);
 
 
     } else {
     	// this is the case for worker processes;
-    	MPI_Status status;
-	    int reqbuf = 0;
-	    int notdoneSign = TRUE; MPI_Request request;
+    	MPI_Status status; MPI_Request request;
+	    int reqbuf = 0, notdoneSign = TRUE;
 	    MPI_Irecv(&notdoneSign,1,MPI_INT,0,STOP_TAG,proc_comm,&request);
-	    
     	while (notdoneSign) {
 	        MPI_Send(&reqbuf, 1, MPI_INT, 0, WORKREQ_TAG, proc_comm);
-	        //printf("the worker %d  has sent reqbuf %d to the manager.\n",rank, reqbuf);
-
 			int result=FALSE;
 			MPI_Test(&request,&result,&status);
             if ( result == TRUE) break;
 			int replyreqbuf = 0;
 			MPI_Recv(&replyreqbuf, 1, MPI_INT, 0, WORK_TAG, proc_comm, &status);
-	        printf("the worker %d has received reply from the manager %d, replyreqbuf  is %d.\n",rank, startRank, replyreqbuf);
-	        
 	        reqbuf = do_some_work(replyreqbuf);
-	        printf("%d is sleeping the %d's %dth times\n", rank, startRank, reqbuf-1);
-
+	        //if (reqbuf > 0) printf("rank %d is doing %dth work\n", rank, reqbuf-1); 
     	}
-    	printf("%d has reached a point to notice manager\n", rank);
     	noticeMyManager(proc_comm);
-    	printf("%d has noticed manager\n", rank);
     }
-    printf("%d is done ==================================\n", rank);
-
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Comm_free(&proc_comm);
     MPI_Comm_free(&mag_comm);
-
     MPI_Finalize();
     return 0;
 }
@@ -247,7 +174,7 @@ int FG_Process(int argc, char **argv){
 int do_some_work(int times)
 {	
 	if (times <= MAXINT && times >=0) {
-		MPIX_Usleep(100); 
+		MPIX_Usleep(100);
 		return times+1;
 	}
 		return -1;
@@ -259,7 +186,6 @@ int finishSendfromworkers(int osProcSize, MPI_Comm proc_comm, int noticeWorker){
 	for (i = 1; i < osProcSize; i++){
 		if (i == noticeWorker) continue;
 		MPI_Request tempReq;
-		printf("the manager is ready to receive from i %d\n", i);
 		MPI_Irecv(&info, 1, MPI_INT, i, WORKREQ_TAG, proc_comm, &tempReq);
 	}
 	return 0;
@@ -268,7 +194,6 @@ int finishSendfromworkers(int osProcSize, MPI_Comm proc_comm, int noticeWorker){
 int noticeMyworkers(int osProcSize, MPI_Comm proc_comm){
 	int i, info = FALSE;
 	for (i = 1; i < osProcSize; i++){
-		printf("i am noticing worker %d\n", i);
 		MPI_Send(&info, 1, MPI_INT, i, STOP_TAG, proc_comm);
 	}
 	return 0;
@@ -299,11 +224,9 @@ int noticeMyPrevMag(int prevmag,MPI_Comm mag_comm){
 int who_are_my_magNeighb(int rank, int osProcNum, int osProcSize, int *prevmag_ptr, int *nextmag_ptr)
 {
     int prevmag = -1, nextmag = -1;
-
  	int procOrder = rank / osProcSize;
     prevmag = (0 ==rank) ? (osProcNum-1) : procOrder-1;
     nextmag = ((osProcNum-1)*osProcSize ==rank) ? 0 : procOrder+1;
-
     *prevmag_ptr = prevmag;
     *nextmag_ptr = nextmag;
     return (0);
